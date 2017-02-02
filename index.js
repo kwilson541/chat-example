c74e3cbefd53dd07bae4e568994150979a107438
Kim Wilson <k.wilson541@gmail.com>
2017-02-02 14:16:38 +0000

    Socket.io listens for messages to be submitted

--- index.js
+++ index.js
@@ -11,9 +11,8 @@ app.get('/', function(req, res){
 
 // Listen on the connection event for incoming sockets
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected')
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
   });
 });
 

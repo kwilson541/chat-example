96766f7fbf19b2f8711ea1568fbc3e47b9e3892a
Kim Wilson <k.wilson541@gmail.com>
2017-02-02 14:02:46 +0000

    Added socket.io, listens for a user to connect

--- index.js
+++ index.js
@@ -2,10 +2,16 @@
 // that you can supply to an HTTP server
 var app = require('express')();
 var http = require('http').Server(app)
+var io = require('socket.io')(http);
 
 // Define a route handler
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+// Listen on the connection event for incoming sockets
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
 
 // Make the HTTP server listen on port 3000
